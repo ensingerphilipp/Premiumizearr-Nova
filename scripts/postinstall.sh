d6d3bf78127bf91e89954e8fb2b51ec12910ae13
ensingerphilipp <philippensi2@gmail.com>
2025-08-07 21:13:07 +0000

    Use default 1000:1000 GID PGID if not otherwise specified

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-chown -R $PUID:$PGID /opt/premiumizearrd/
+# Set default UID and GID if not provided
+PUID=${PUID:-1000}
+PGID=${PGID:-1000}
+
+# Set permissions
+chown -R "$PUID:$PGID" /opt/premiumizearrd/
+
+# Replace placeholders in systemd service file
 sed -i "" "s/###USER###/$PUID/g" /etc/systemd/system/premiumizearrd.service
 sed -i "" "s/###GROUP###/$PGID/g" /etc/systemd/system/premiumizearrd.service
+
+# Enable and start the service
 systemctl enable premiumizearrd.service
 systemctl daemon-reload
 systemctl start premiumizearrd.service
